e7bbe1763465c1a3c762c00ce9bad0f12ba67591
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-05-22 00:42:18 +0300

    Add type hints and get_max_flight_minutes to Airplane model

--- airplane/models.py
+++ airplane/models.py
@@ -4,14 +4,17 @@ from django.db import models
 
 
 class Airplane(models.Model):
-    name = models.CharField(max_length=100, nullable=False)
+    name = models.CharField(max_length=100)
     passengers = models.IntegerField(default=0)
 
-    def get_fuel_tank_capacity(self):
+    def get_fuel_tank_capacity(self) -> int:
         return 200 * self.id
 
-    def get_fuel_consumption(self):
-        return log(self.id) * 0.80
+    def get_fuel_consumption(self) -> float:
+        return log(self.id * 0.80)
 
-    def get_actual_fuel_consumption(self):
+    def get_actual_fuel_consumption(self) -> float:
         return self.get_fuel_consumption() + (self.passengers * 0.002)
+
+    def get_max_flight_minutes(self):
+        return self.get_fuel_tank_capacity() // self.get_actual_fuel_consumption()
